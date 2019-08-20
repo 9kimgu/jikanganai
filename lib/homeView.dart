1603cac930c0915e029b7517cd3faaf308c31ba2
B <9kimgu>
2019-08-20 06:27:13 -0500

    converted homescreen to customlist of slivers, removed universal appbar

--- lib/homeView.dart
+++ lib/homeView.dart
@@ -13,38 +13,47 @@ class PlaceholderWidget2 extends StatelessWidget {
   }
 }
 
-
 class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20.0),
-      height: 200.0,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
+    return CustomScrollView(slivers: <Widget>[
+      const SliverAppBar(
+        pinned: false,
+        expandedHeight: 40.0,
+        flexibleSpace: FlexibleSpaceBar(
+          title: Text('Jikan ga nai!'),
+        ),
+      ),
+      SliverList(
+        delegate: SliverChildListDelegate([
           Container(
-            width: 200.0,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 200.0,
-            color: Colors.teal,
-          ),
-          Container(
-            width: 200.0,
-            color: Colors.orange,
-          ),
-          Container(
-            width: 200.0,
-            color: Colors.amber,
-          ),
-          Container(
-            width: 200.0,
-            color: Colors.grey,
-          ),
-        ]
-    )
-    );
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              height: 200.0,
+              child:
+                  ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+                Container(
+                  width: 200.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 200.0,
+                  color: Colors.teal,
+                ),
+                Container(
+                  width: 200.0,
+                  color: Colors.orange,
+                ),
+                Container(
+                  width: 200.0,
+                  color: Colors.amber,
+                ),
+                Container(
+                  width: 200.0,
+                  color: Colors.grey,
+                ),
+              ]))
+        ]),
+      )
+    ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -64,11 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped,

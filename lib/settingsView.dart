d2f6ead2a70ca8e4022d7883dfc1d1c01aecba14
B <9kimgu>
2019-08-20 20:23:21 -0500

    polished settings view

--- lib/settingsView.dart
+++ lib/settingsView.dart
@@ -5,10 +5,27 @@ class SettingsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Settings')),
-        body: ListView(children: const <Widget>[
+        body: ListView(children: <Widget>[
           ListTile(
-            leading: Icon(Icons.brightness_5),
-            title: Text('Screen'),
+            leading: Icon(Icons.format_paint),
+            title: Text('Appearance'),
+            onTap: (){
+
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            onTap: (){
+
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.insert_drive_file),
+            title: Text('Info'),
+            onTap: (){
+
+            },
           ),
         ]));
   }

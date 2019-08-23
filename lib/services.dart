db5104b0f78b7e0c17c1f99091642828555ca95a
B <9kimgu>
2019-08-23 05:32:09 -0500

    changed top anime/airing to current season

--- lib/services.dart
+++ lib/services.dart
@@ -1,3 +1,11 @@
+import 'package:jikanganai/currentA_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:io';
 
 String url = 'https://api.jikan.moe/v3/';
 
+Future<CurrentSeason> getTopAnime() async{
+  final response = await http.get('$url/top/anime/airing');
+  return currentSeasonFromJson(response.body);
+}

a0336821a9b06ce7d7ddcacd65e99282cebfd221
B <9kimgu>
2019-08-29 08:58:48 -0500

    details screen added to listview elements

--- lib/homeView.dart
+++ lib/homeView.dart
@@ -4,6 +4,7 @@ import 'package:jikanganai/currentA_model.dart';
 import 'package:jikanganai/services.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'dart:io';
+import 'package:jikanganai/details.dart';
 
 class PlaceholderWidget2 extends StatelessWidget {
   final Color color;
@@ -18,7 +19,7 @@ class PlaceholderWidget2 extends StatelessWidget {
   }
 }
 
-class HomeWidget extends StatelessWidget{
+class HomeWidget extends StatelessWidget {
   Future<CurrentSeason> _loadAnime;
   // @override
   // void initState(){
@@ -46,49 +47,50 @@ class HomeWidget extends StatelessWidget{
                 style: Theme.of(context).textTheme.title,
               )),
           Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0),
-              height: 200.0,
-              child:
-                    FutureBuilder<CurrentSeason>(
-                        future: getCurrentAnime(),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                              return ListView.builder(scrollDirection: Axis.horizontal,
-                              itemCount: 10,
-                              itemBuilder:(context, index) => Padding(
-                                padding: EdgeInsets.all(8.0), 
-                                child: Container(
-                                child: FadeInImage.memoryNetwork(
-                                  placeholder: kTransparentImage,
-                                  image: '${snapshot.data.anime[index].imageUrl}'
-                                )
-                              ),
+            margin: EdgeInsets.symmetric(vertical: 10.0),
+            height: 200.0,
+            child: FutureBuilder<CurrentSeason>(
+                future: getCurrentAnime(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        itemBuilder: (context, index) => Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: GestureDetector(
+                                  onTap: () {
+                                    toDetailsScreen(context);
+                                  },
+                                  child: FadeInImage.memoryNetwork(
+                                      placeholder: kTransparentImage,
+                                      image:
+                                          '${snapshot.data.anime[index].imageUrl}')),
                             ));
-                          } else {
-                            return Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                        }),
-                  //ListView(scrollDirection: Axis.horizontal, children: <Widget>[
-                //Container(
-                    // child: FutureBuilder<CurrentSeason>(
-                    //     future: getCurrentAnime(),
-                    //     builder: (context, snapshot) {
-                    //       if (snapshot.connectionState ==
-                    //           ConnectionState.done) {
-                    //         return FadeInImage.memoryNetwork(
-                    //           placeholder: kTransparentImage,
-                    //           image: '${snapshot.data.anime[0].imageUrl}',
-                    //         );
-                    //       } else {
-                    //         return Center(
-                    //           child: CircularProgressIndicator(),
-                    //         );
-                    //       }
-                    //     })),
-              ),
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+            //ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+            //Container(
+            // child: FutureBuilder<CurrentSeason>(
+            //     future: getCurrentAnime(),
+            //     builder: (context, snapshot) {
+            //       if (snapshot.connectionState ==
+            //           ConnectionState.done) {
+            //         return FadeInImage.memoryNetwork(
+            //           placeholder: kTransparentImage,
+            //           image: '${snapshot.data.anime[0].imageUrl}',
+            //         );
+            //       } else {
+            //         return Center(
+            //           child: CircularProgressIndicator(),
+            //         );
+            //       }
+            //     })),
+          ),
           Padding(
               padding: EdgeInsets.all(8.0),
               child: Text(
